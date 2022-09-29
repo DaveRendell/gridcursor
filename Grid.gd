fb7697726f1d22a7e0fe2d96dd79c55daf925df2
David Rendell <davidwrendell@gmail.com>
2022-09-29 18:37:08 +0100

    Consolidate into single grid script

--- Grid.gd
+++ Grid.gd
@@ -7,24 +7,48 @@ export var grid_height: int = 7
 var width = grid_width * grid_size
 var height = grid_height * grid_size
 
+# Cursor properties
+var cursor_x: int = 0
+var cursor_y: int = 0
+var using_mouse = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Cursor.grid_size = grid_size
-	$Cursor.grid_width = grid_width
-	$Cursor.grid_height = grid_height
-	$Cursor.grid_global_position = global_position
 	draw_grid()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	$Cursor.position.x = cursor_x * grid_size
+	$Cursor.position.y = cursor_y * grid_size
+	if using_mouse:
+		set_position_to_mouse_cursor()
+
+
+func set_position_to_mouse_cursor():
+	var mouse_relative_position = get_global_mouse_position() - global_position
+	cursor_x = mouse_relative_position.x / grid_size
+	cursor_y = mouse_relative_position.y / grid_size
+
 
 func _input(event):
+	if event.is_action_pressed("ui_up") and cursor_y > 0:
+		cursor_y -= 1
+		using_mouse = false
+	if event.is_action_pressed("ui_down") and cursor_y < grid_height - 1:
+		cursor_y += 1
+		using_mouse = false
+	if event.is_action_pressed("ui_left") and cursor_x > 0:
+		cursor_x -= 1
+		using_mouse = false
+	if event.is_action_pressed("ui_right") and cursor_x < grid_width - 1:
+		cursor_x += 1
+		using_mouse = false
+	
 	if (event.is_action_pressed("ui_accept"))\
 	|| (event is InputEventMouseButton and event.is_pressed()):
-		click_position($Cursor.x, $Cursor.y)
+		click_position(cursor_x, cursor_y)
 
 
 func click_position(x, y):
@@ -53,3 +77,12 @@ func draw_grid():
 		line.add_point(Vector2(0, offset))
 		line.add_point(Vector2(width, offset))
 		add_child(line)
+
+
+# Signals
+func _on_Background_mouse_entered():
+	using_mouse = true
+
+
+func _on_Background_mouse_exited():
+	using_mouse = false
