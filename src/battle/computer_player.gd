32dd50e74d67420812af0672ac6f49c3f23d1916
David Rendell <davidwrendell@gmail.com>
2022-10-20 21:52:30 +0100

    Monsters move towards player characters if out of range

--- src/battle/computer_player.gd
+++ src/battle/computer_player.gd
@@ -33,18 +33,50 @@ static func execute_unit_turn(unit: Unit, map: Map, unit_turn: UnitTurnComplete)
 		print(unit.character.attacks())
 		print(attack_source)
 		var attack = unit.character.attacks()[attack_source.attack_id]
-		var path = unit.get_path_to_coords(map, attack_source.source)
 		
-		map.path = path
+		map.path = unit.get_path_to_coords(map, attack_source.source)
 		var animation = unit.animate_movement_along_path(map)
 		yield(animation, "finished")
 		
 		var popup = unit.perform_attack(map, target, attack)
 		yield(popup, "tree_exited")
 		
-		unit.update_position(map, path.last())
+		unit.update_position(map, attack_source.source)
+	else:
+		var edge_of_movement_range = edge_of_movement_range(unit)
+		var nearest_distance = INF
+		var best_location: Coordinate
+		for cell in edge_of_movement_range.to_array():
+			var nearest_distance_at_cell = distance_to_nearest_enemy(cell, unit, map)
+			if nearest_distance_at_cell < nearest_distance:
+				nearest_distance = nearest_distance_at_cell
+				best_location = cell
 		
+		map.path = unit.get_path_to_coords(map, best_location)
+		print(map.path)
+		var animation = unit.animate_movement_along_path(map)
+		yield(animation, "finished")
+		
+		unit.update_position(map, best_location)
+				
 	unit.state_to_done(map)
 	# Brief pause for effect...
 	yield(unit.get_tree().create_timer(0.25), "timeout")	
 	return unit_turn.emit_signal("complete", unit)
+
+static func edge_of_movement_range(unit: Unit) -> CoordinateList:
+	var out = []
+	for coordinate in unit.remaining_movement_at_cell.coordinates():
+		if unit.remaining_movement_at_cell.at(coordinate) == 0:
+			out.append(coordinate)
+	return CoordinateList.new(out)
+
+static func distance_to_nearest_enemy(position: Coordinate, unit: Unit, map: Map) -> int:
+	var nearest_distance = INF
+	for child in map.get_node("GridNodes").get_children():
+		var other_unit = child as Unit
+		if other_unit and other_unit.team != unit.team:
+			var distance = map.distance(unit.coordinate(), other_unit.coordinate())
+			nearest_distance = min(nearest_distance, distance)
+	return nearest_distance
+			
